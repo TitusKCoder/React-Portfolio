830901b718a21e07c97d32e3d7cc3a14a37c5590
Titus Knox <knoxtitus@gmail.com>
2022-04-29 09:13:15 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.32da48ca3c948821ef4a69e93a15b7d8.js"
+  "/React-Portfolio/precache-manifest.85df010292d224b5f47c8beb362aa574.js"
 );
 
 workbox.clientsClaim();
