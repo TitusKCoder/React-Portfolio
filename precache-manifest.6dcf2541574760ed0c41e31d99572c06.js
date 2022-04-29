1a8af2a5ab271dfd6fb07ed2c7f6dc4782beef86
Titus Knox <knoxtitus@gmail.com>
2022-04-29 09:20:14 -0400

    Updates

---
+++ precache-manifest.6dcf2541574760ed0c41e31d99572c06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "927b9af12b7bcf291fee",
+    "url": "/React-Portfolio/static/js/main.fc86b877.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "3169c703b3db00284903",
+    "url": "/React-Portfolio/static/css/2.9d4f3f60.chunk.css"
+  },
+  {
+    "revision": "3169c703b3db00284903",
+    "url": "/React-Portfolio/static/js/2.17b4ed91.chunk.js"
+  },
+  {
+    "revision": "f2b8067d1182e2e7b26ea66649f5f391",
+    "url": "/React-Portfolio/index.html"
+  }
+];
