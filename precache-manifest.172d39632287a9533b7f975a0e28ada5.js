daf3a0685cb7f8c93deb7fca2f8ee3d76dbe15aa
Titus Knox <knoxtitus@gmail.com>
2022-08-10 12:40:25 -0400

    Updates

---
+++ precache-manifest.172d39632287a9533b7f975a0e28ada5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8d5ba2db976d56a3b15",
+    "url": "/React-Portfolio/static/css/main.606c4610.chunk.css"
+  },
+  {
+    "revision": "f8d5ba2db976d56a3b15",
+    "url": "/React-Portfolio/static/js/main.962d7178.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  },
+  {
+    "revision": "d4e6b1057d7bb3efc31d",
+    "url": "/React-Portfolio/static/css/2.9d4f3f60.chunk.css"
+  },
+  {
+    "revision": "d4e6b1057d7bb3efc31d",
+    "url": "/React-Portfolio/static/js/2.b1ed2dbf.chunk.js"
+  },
+  {
+    "revision": "2c6f8504beb908d34cc6f58c56913f6d",
+    "url": "/React-Portfolio/static/media/Resume.2c6f8504.PNG"
+  },
+  {
+    "revision": "08e347e33d60868f5600728c2b6f87e4",
+    "url": "/React-Portfolio/static/media/1517357457027.08e347e3.jpg"
+  },
+  {
+    "revision": "615e6202cca14ae5066832a94c4ab233",
+    "url": "/React-Portfolio/static/media/profilepic22.615e6202.png"
+  },
+  {
+    "revision": "48acf2e57531dcd6d6905d1af43fa402",
+    "url": "/React-Portfolio/static/media/Dayplanner.48acf2e5.PNG"
+  },
+  {
+    "revision": "e599811953a5928806baf632f568b9f5",
+    "url": "/React-Portfolio/static/media/EcommBackend.e5998119.PNG"
+  },
+  {
+    "revision": "54331e40b4c6918945945b53a55085f6",
+    "url": "/React-Portfolio/static/media/timesquare.54331e40.PNG"
+  },
+  {
+    "revision": "5c4a46081c7a181b468afb868cbd56d3",
+    "url": "/React-Portfolio/index.html"
+  }
+];
